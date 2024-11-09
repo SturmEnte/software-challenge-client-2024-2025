809d463a32e99aa3b752bba29ac08be9c0877c61
Jonas <jonas@sturmente.de>
2024-11-09 09:55:58 +0100

    Update main.rs

--- rust/simple-client/src/main.rs
+++ rust/simple-client/src/main.rs
@@ -19,31 +19,37 @@ fn main() {
     // Send join message
     stream.write(join_msg.as_bytes()).unwrap();
 
+    let mut file = File::create("buffer.txt").unwrap();
+
+    // Continuesly read messages from the server and react to them
     loop {
         let mut buffer: [u8; 5000] = [0; 5000];
-
         let n: usize = stream.read(&mut buffer[..]).unwrap();
         
-        if buffer.starts_with(b"<protocol>") {
-            println!("Joined room");
-            //game_data.lock().unwrap().room_id = get_room_id(&buffer);
-            //println!("Room id: {}", game_data.lock().unwrap().room_id);
-            continue;
-        } else if buffer[n-7..n] == "</room>".as_bytes().to_owned() { // returns true, if the data in the buffer ends with </room>
-            global_buffer.write(&buffer[..n]).unwrap();
-            global_n += n;
+        // Write buffer to file for debugging
+        
+        file.write(&buffer[..n]).unwrap();
 
-            /*let game_end: bool = parse_message(global_buffer.into_inner(), global_n, &game_data, &mut Some(&mut stream));
+        // if buffer.starts_with(b"<protocol>") {
+        //     println!("Joined room");
+        //     //game_data.lock().unwrap().room_id = get_room_id(&buffer);
+        //     //println!("Room id: {}", game_data.lock().unwrap().room_id);
+        //     continue;
+        // } else if buffer[n-7..n] == "</room>".as_bytes().to_owned() { // returns true, if the data in the buffer ends with </room>
+        //     global_buffer.write(&buffer[..n]).unwrap();
+        //     global_n += n;
 
-            if game_end {
-                break;
-            }*/
+        //     /*let game_end: bool = parse_message(global_buffer.into_inner(), global_n, &game_data, &mut Some(&mut stream));
 
-            global_buffer = Cursor::new([0; 5000]);
-            global_n = 0usize;
+        //     if game_end {
+        //         break;
+        //     }*/
+
+        //     global_buffer = Cursor::new([0; 5000]);
+        //     global_n = 0usize;
             
-            continue;
-        } 
+        //     continue;
+        // } 
 
         // Add buffer data to the global buffer and add n to the global n
         global_buffer.write(&buffer[..n]).unwrap();
