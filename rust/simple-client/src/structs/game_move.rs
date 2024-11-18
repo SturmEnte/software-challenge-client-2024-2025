a58e23dfafd24253e5b2e998b4e43e082567d283
Jonas <jonas@sturmente.de>
2024-11-18 21:27:26 +0100

    Implemented "new" method in move

--- rust/simple-client/src/structs/game_move.rs
+++ rust/simple-client/src/structs/game_move.rs
@@ -3,3 +3,11 @@ use crate::structs::action::Action;
 pub struct Move {
     pub actions: Vec<Box<dyn Action>>,
 }
+
+impl Move {
+    pub fn new(actions: Vec<Box<dyn Action>>) -> Move {
+        Move {
+            actions: actions,
+        }
+    }
+}
