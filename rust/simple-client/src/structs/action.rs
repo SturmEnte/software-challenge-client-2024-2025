e0f89b8a6a5a00f130ce6475c02ed337af51beb0
Jonas <jonas@sturmente.de>
2024-11-18 21:27:12 +0100

    Implemented advance action

--- rust/simple-client/src/structs/action.rs
+++ rust/simple-client/src/structs/action.rs
@@ -6,6 +6,14 @@ pub struct Advance {
     pub distance: i8,
 }
 
+impl Advance {
+    pub fn new(distance: i8) -> Advance {
+        Advance {
+            distance: distance,
+        }
+    }
+}
+
 impl Action for Advance {
     fn to_string(&self, index: &i8) -> String {
         format!("<advance order=\"{}\" distance=\"{}\"/>", index, self.distance)
