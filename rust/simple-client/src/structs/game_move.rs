e76566d3a68efd6079736f0f3f733bac6a932b2a
SturmEnte <sturmente@proton.me>
2024-11-19 00:31:05 +0100

    Deleted action file

--- rust/simple-client/src/structs/game_move.rs
+++ rust/simple-client/src/structs/game_move.rs
@@ -34,3 +34,26 @@ impl Move for AdvanceMove {
 //         }
 //     }
 // }
+
+// Old action file:
+/*pub trait Action {
+    fn to_string(&self, index: &i8) -> String; 
+}
+
+pub struct Advance {
+    pub distance: i8,
+}
+
+impl Advance {
+    pub fn new(distance: i8) -> Advance {
+        Advance {
+            distance: distance,
+        }
+    }
+}
+
+impl Action for Advance {
+    fn to_string(&self, index: &i8) -> String {
+        format!("<advance order=\"{}\" distance=\"{}\"/>", index, self.distance)
+    }
+}*/
