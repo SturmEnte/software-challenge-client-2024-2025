cb52f7cd9943fe451667acf035d2a5c9657feba7
XWalkuereX <marek.kian.walczynski@gmail.com>
2024-11-20 19:48:06 +0100

    Connect to server

---
+++ python/simple-client-marek/connect.py
@@ -0,0 +1,38 @@
+
+import socket
+
+
+
+class ServerConnection ():
+
+    def __init__(self, hostname = 'localhost', socketNummber = 13050):
+        self.hostname = hostname
+        self.socketNummber = socketNummber
+        self.RId = None
+        self.resCode = None
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((hostname, socketNummber))
+        
+        
+
+    def joinRoom(self):
+             
+        if(self.RId == None and self.resCode == None):
+            self.client.send("<protocol><join gameType='swc_2025_hase_und_igel'/>".encode())
+
+            pass
+        elif(self.RId != None and self.resCode == None):
+            self.client.send("<protocol><joinRoom roomId={RId}>".encode())
+            pass
+        else:
+            self.client.send("<protocol><joinPrepared reservationCode={resCode}>".encode())
+            pass
+
+    def receiveData(self):
+        data = self.client.recv()
+        print(data.decode())
+        pass
+
+# Temporary Main
+serverConnection = ServerConnection()
+serverConnection.joinRoom()
