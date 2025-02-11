6f25a1bfc51738f2fb9eccb6401f1db4da60a404
Unbekannt28 <mika@schiessler.net>
2025-02-11 18:07:53 +0100

    implemented test_compute for new client

--- python/simpleclient1/test_compute.py
+++ python/simpleclient1/test_compute.py
@@ -1,10 +1,13 @@
-from parse_xml import parseMementoStart # parseMementoStart is imported, because it also returns the start team
+from parse_xml import parse_memento_start # parse_memento_start is imported, because it also returns the start team
 from time import time
 from state import State
 from xml.etree.ElementTree import fromstring
-from compute import computeMove
+from compute import compute_move, get_possible_moves
 
-with open("../test/memento9.xml", "r") as msg:
+#TODO: make it work with entire replay and specify turn per variable/input
+#      this has to be done to correctly get the last move for eat_salad calculation and for last use of exchange carrots card
+
+with open("../test/memento0.xml", "r") as msg:
     msg = msg.read()
     print("\nNEW MESSAGE:\n" + msg + "\n" + "-"*35)
 
@@ -17,12 +20,18 @@ with open("../test/memento9.xml", "r") as msg:
     xmlState = data.find('state')
     turn = int(xmlState.attrib['turn'])
 
-    startTeam, board, nextDirection, players = parseMementoStart(xmlState)
-    state = State("TWO", turn, startTeam, board, nextDirection, players)
+    start_team, board, players = parse_memento_start(xmlState)
+    state = State("TWO", turn, start_team, board, players)
 
-    move = computeMove(state)
+    move = compute_move(state)
 
     t2 = time()
     print(f"Zeit: {t2-t1}   Zug: {turn}")
-    state.printState()
+    state.print_state()
     print(move)
+
+    print("-"*35)
+    print("Possible Moves:")
+    pmvs = get_possible_moves(state)
+    for mv in pmvs:
+        print(mv)
