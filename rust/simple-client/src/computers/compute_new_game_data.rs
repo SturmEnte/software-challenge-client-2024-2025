ec62ff5db5ffc5d66cd50cad22e496618a7da889
Jonas <jonas@sturmente.de>
2025-02-12 17:07:16 +0100

    Update compute_new_game_data.rs

--- rust/simple-client/src/computers/compute_new_game_data.rs
+++ rust/simple-client/src/computers/compute_new_game_data.rs
@@ -1,9 +1,25 @@
-use crate::structs::{game_data::GameData, game_move::Move};
+use crate::{enums::move_type::MoveType, structs::{game_data::GameData, game_move::{AdvanceMove, Move}, hare::Hare}};
 
 pub fn compute_new_game_data(game_data: &GameData, m: &Box<dyn Move>, our_hares_move: &bool) -> GameData {
-    let mut new_game_data = game_data.clone();
+    let mut new_game_data: GameData = game_data.clone();
 
-    println!("Our hare's move: {}", our_hares_move);
+    let current_hare: &mut Hare = if *our_hares_move { &mut new_game_data.our_hare } else { &mut new_game_data.enemy_hare };
+    let other_hare: &mut Hare = if *our_hares_move { &mut new_game_data.enemy_hare } else { &mut new_game_data.our_hare };
+
+    match m.get_type() {
+        MoveType::Advance => {
+            current_hare.position += m.distance;
+        },
+        MoveType::EatSalad => {
+
+        },
+        MoveType::ExchangeCarrots => {
+
+        },
+        MoveType::Fallback => {
+
+        }
+    }
 
     new_game_data
 }
