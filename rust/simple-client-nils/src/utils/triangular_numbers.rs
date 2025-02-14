c7bfa540d0a4f7623922f255cfc6643111b84a0b
NichtNil5 <nilsmarten2005@gmail.com>
2025-02-14 12:29:30 +0100

    added calculate_reverse_triangular_number_floor

--- rust/simple-client-nils/src/utils/triangular_numbers.rs
+++ rust/simple-client-nils/src/utils/triangular_numbers.rs
@@ -1,3 +1,7 @@
 pub fn calculate_triangular_number(n: u16) -> u16 {
     n * (n + 1) /2
 }
+
+pub fn calculate_reverse_triangular_number_floor(n: u16) -> u16 {
+    (((n * 8) as f64 - 1.0).sqrt() / 2.0).floor() as u16
+}
