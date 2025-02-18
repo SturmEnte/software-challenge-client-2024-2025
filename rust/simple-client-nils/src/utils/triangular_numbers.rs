ab21b28076dbef14be9c7acef06aac567901bbc5
NichtNil5 <nilsmarten2005@gmail.com>
2025-02-18 16:59:00 +0100

    The calculate_reverse_triangular_number_floor function now returns the right result

--- rust/simple-client-nils/src/utils/triangular_numbers.rs
+++ rust/simple-client-nils/src/utils/triangular_numbers.rs
@@ -3,5 +3,5 @@ pub fn calculate_triangular_number(n: u16) -> u16 {
 }
 
 pub fn calculate_reverse_triangular_number_floor(n: u16) -> u16 {
-    (((n * 8) as f64 - 1.0).sqrt() / 2.0).floor() as u16
+    return ((-1.0 + ((1.0 + 8.0 * n as f64).sqrt())) / 2.0).floor() as u16;
 }
