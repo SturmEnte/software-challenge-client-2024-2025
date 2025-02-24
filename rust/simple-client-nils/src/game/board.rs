c8409e8e81ec038fdb0c10e0425bcfea195951a8
NichtNil5 <nilsmarten2005@gmail.com>
2025-02-24 14:36:58 +0100

    The constant HEDGEHOG_FIELDS,  SALAD_FIELDS, FIRST_HEADGEHOG and LAST_SALAD have been added

--- rust/simple-client-nils/src/game/board.rs
+++ rust/simple-client-nils/src/game/board.rs
@@ -1,5 +1,10 @@
 use super::field_type::FieldType;
 
+pub const HEDGEHOG_FIELDS: [u8; 9] = [11, 15, 19, 24, 30, 37, 43, 50, 56];
+pub const SALAD_FIELDS: [u8; 4] = [10, 22, 42, 57];
+pub const FIRST_HEADGEHOG: u8 = 11;
+pub const LAST_SALAD: u8 = 57;
+
 pub struct Board {
     pub board: [FieldType; 65]
 }
