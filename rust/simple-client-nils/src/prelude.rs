9a23b88c549d87c2e95870e28a2cf8224c82165c
NichtNil5 <nilsmarten2005@gmail.com>
2025-02-24 15:57:07 +0100

    Added prelude.

---
+++ rust/simple-client-nils/src/prelude.rs
@@ -0,0 +1,3 @@
+pub use crate::connection_handler::connection_handler::ConnectionHandler;
+pub use crate::computer_player::ComputerPlayer;
+pub use crate::game::{Board, GameState, GameMove, Hare, FieldType, legal_moves::calculate_legal_moves};
