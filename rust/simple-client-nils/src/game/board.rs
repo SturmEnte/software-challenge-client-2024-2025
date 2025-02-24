2c5260f439755bae9c484d720a2d7304427eeed3
NichtNil5 <nilsmarten2005@gmail.com>
2025-02-24 18:11:41 +0100

    Implemented Display on FieldType and Board.

--- rust/simple-client-nils/src/game/board.rs
+++ rust/simple-client-nils/src/game/board.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use super::field_type::FieldType;
 
 pub const HEDGEHOG_FIELDS: [u8; 9] = [11, 15, 19, 24, 30, 37, 43, 50, 56];
@@ -15,4 +17,14 @@ impl Board {
             board: board
         }
     }
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, field) in self.board.iter().enumerate() {
+            write!(f, "[{}|{}]", i, field)?;
+        }
+        Ok(())
+    } 
+
 }
--- rust/simple-client-nils/src/game/field_type.rs
+++ rust/simple-client-nils/src/game/field_type.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 pub enum FieldType {
     Start,
@@ -9,4 +11,20 @@ pub enum FieldType {
     Market,
     Hare,
     Goal,
+}
+
+impl Display for FieldType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FieldType::Start => write!(f, "🏳️‍⚧️"),
+            FieldType::Carrots => write!(f, "🥕"),
+            FieldType::Salad => write!(f, "🥬"),
+            FieldType::Position1 => write!(f, "1️⃣"),
+            FieldType::Position2 => write!(f, "2️⃣"),
+            FieldType::Hedgehog => write!(f, "🦔"),
+            FieldType::Market => write!(f, "🏪"),
+            FieldType::Hare => write!(f, "🐰"),
+            FieldType::Goal => write!(f, "🏁"),
+        }
+    }
 }
