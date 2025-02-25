0fe008ffe5b68e2adf29d27175f40465214f54fe
Unbekannt28 <mika@schiessler.net>
2025-02-25 17:58:42 +0100

    initial commit

--- python/hot-n-spicy-tuff-golem/compute.py
+++ python/hot-n-spicy-tuff-golem/compute.py
@@ -1,9 +1,14 @@
 from random import choice
+from player import Player
 
 from move import Move
 from time import time
+from copy import deepcopy
 
 TIME_LIMIT = 1.8
+LAST_SALAD_FIELD = 57
+LAST_FIELD = 64
+FIRST_FIELD = 1
 
 def get_needed_carrots(n):
     '''Rennkarte'''
@@ -24,6 +29,71 @@ def get_hedgehog_field(state, position):
             return field, position - field_index
     
     return None, None
+
+def get_moves_recursive(state, player, opponent, index, incomplete_move):
+    #needed_carrots = get_needed_carrots(index - player.position)
+    moves = []
+    field = state.board.get_field(index)
+
+    if field.type == "HEDGEHOG":
+        return []
+    
+    if opponent.position == index and field.type != "GOAL":
+        return []
+    
+    if field.type == "GOAL":
+        if player.carrots > 10 or player.salads > 0:
+            return []
+    
+    if field.type == "MARKET":
+        if player.carrots < 10:
+            return []
+        for card in ("SWAP_CARROTS", "EAT_SALAD", "HURRY_AHEAD", "FALL_BACK"):
+            move = deepcopy(incomplete_move)
+            move.append_card(card)
+            moves.append(move)
+        return moves
+    
+    if field.type == "HARE":
+        if len(player.cards) < 1:
+            return []
+        if "SWAP_CARROTS" in player.cards:
+            if index < LAST_SALAD_FIELD and opponent.position < LAST_SALAD_FIELD and state.turn - state.last_swap_carrots_turn > 2:
+                move = deepcopy(incomplete_move)
+                move.append_card("SWAP_CARROTS")
+                moves.append(move)
+        
+        if "EAT_SALAD" in player.cards:
+            if player.salads > 0:
+                move = deepcopy(incomplete_move)
+                move.append_card("EAT_SALAD")
+                moves.append(move)
+        
+        if "HURRY_AHEAD" in player.cards:
+            if index < opponent.position:
+                move = deepcopy(incomplete_move)
+                move.append_card("HURRY_AHEAD")
+                new_player = Player(player.team, index, player.salads, player.carrots)
+                new_player.cards += player.cards
+                new_player.cards.remove("HURRY_AHEAD")
+                moves += get_moves_recursive(state, new_player, opponent, opponent.position + 1, move)
+        
+        if "FALL_BACK" in player.cards:
+            if index > opponent.position:
+                move = deepcopy(incomplete_move)
+                move.append_card("FALL_BACK")
+                new_player = Player(player.team, index, player.salads, player.carrots)
+                new_player.cards += player.cards
+                new_player.cards.remove("FALL_BACK")
+                moves += get_moves_recursive(state, new_player, opponent, opponent.position - 1, move)
+
+
+        return moves
+    
+    moves.append(incomplete_move)
+    return moves
+
+
 
 def get_possible_moves(state, use_opponent=False):
     if use_opponent:
@@ -51,7 +121,7 @@ def get_possible_moves(state, use_opponent=False):
         pmvs.append(move)
     
     # exchange carrots move
-    if state.board.get_field(player.position).type == "CARROT":
+    if state.board.get_field(player.position).type == "CARROTS":
         move = Move()
         move.exchange_carrots(10)
         pmvs.append(move)
@@ -69,17 +139,60 @@ def get_possible_moves(state, use_opponent=False):
         if field.type == "HEDGEHOG":
             continue
 
-        if opponent.position == i:
+        if opponent.position == i and field.type != "GOAL":
             continue
 
         if field.type == "GOAL":
-            if player.carrots > 10 or player.salads > 0:
+            if player.carrots - needed_carrots > 10 or player.salads > 0:
                 continue
 
         if field.type == "HARE": #TODO: check hare field conditions
+            if len(player.cards) < 1:
+                continue
+            if "SWAP_CARROTS" in player.cards:
+                if i < LAST_SALAD_FIELD and opponent.position < LAST_SALAD_FIELD and state.turn - state.last_swap_carrots_turn > 2 and needed_carrots <= player.carrots:
+                    move = Move()
+                    move.advance(i - player.position)
+                    move.append_card("SWAP_CARROTS")
+                    pmvs.append(move)
+            
+            if "EAT_SALAD" in player.cards:
+                if player.salads > 0 and needed_carrots <= player.carrots:
+                    move = Move()
+                    move.advance(i - player.position)
+                    move.append_card("EAT_SALAD")
+                    pmvs.append(move)
+            
+            if "HURRY_AHEAD" in player.cards:
+                if i < opponent.position and needed_carrots <= player.carrots and opponent.position != LAST_FIELD:
+                    move = Move()
+                    move.advance(i - player.position)
+                    move.append_card("HURRY_AHEAD")
+                    new_player = Player(player.team, i, player.salads, player.carrots - needed_carrots)
+                    new_player.cards += player.cards
+                    new_player.cards.remove("HURRY_AHEAD")
+                    pmvs += get_moves_recursive(state, new_player, opponent, opponent.position + 1, move)
+            
+            if "FALL_BACK" in player.cards:
+                if i > opponent.position and needed_carrots <= player.carrots and opponent.position != FIRST_FIELD:
+                    move = Move()
+                    move.advance(i - player.position)
+                    move.append_card("FALL_BACK")
+                    new_player = Player(player.team, i, player.salads, player.carrots - needed_carrots)
+                    new_player.cards += player.cards
+                    new_player.cards.remove("FALL_BACK")
+                    pmvs += get_moves_recursive(state, new_player, opponent, opponent.position - 1, move)
+
             continue
 
-        if field.type == "MARKET": #TODO: check market field conditions
+        if field.type == "MARKET":
+            if player.carrots < 10 + needed_carrots:
+                continue
+            for card in ("SWAP_CARROTS", "EAT_SALAD", "HURRY_AHEAD", "FALL_BACK"):
+                move = Move()
+                move.advance(i - player.position)
+                move.append_card(card)
+                pmvs.append(move)
             continue
 
         if (needed_carrots <= player.carrots):
