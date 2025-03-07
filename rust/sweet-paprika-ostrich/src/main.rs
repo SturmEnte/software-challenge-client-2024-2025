0f41cf6e5bb64908d6610143894188f7deeaa963
NichtNil5 <nilsmarten2005@gmail.com>
2025-03-07 10:57:06 +0100

    Adjusted evaluation

--- rust/sweet-paprika-ostrich/src/main.rs
+++ rust/sweet-paprika-ostrich/src/main.rs
@@ -67,10 +67,15 @@ fn minimax(game_state: &GameState, board: &Board, depth: usize, maximizing_playe
 fn eval(game_state: &GameState, board: &Board) -> i32 {
     if game_state.your_hare.position == 64 {return std::i32::MAX;}
     let mut eval = 0;
-    if game_state.opponent_hare.position == 64 {eval -= 200000}
+    if game_state.opponent_hare.position == 64 {eval -= 2000000}
     eval += game_state.opponent_hare.salads as i32 * 5;
-    eval -= game_state.your_hare.salads as i32 * 150;
-    if game_state.your_hare.salads != 0 {eval += (game_state.your_hare.card_eat_salad as i32 * 50).clamp(0, 100)} 
+    eval -= game_state.your_hare.salads as i32 * 130;
+    if game_state.your_hare.salads != 0 {eval += match game_state.your_hare.card_eat_salad{
+        0 => 0,
+        1 => 100,
+        2 => 150,
+        _ => 130
+    }} 
     else {eval += game_state.your_hare.position as i32 * 5}
     eval += match game_state.your_hare.position {
         0..20 => game_state.your_hare.position as i32,
@@ -78,5 +83,6 @@ fn eval(game_state: &GameState, board: &Board) -> i32 {
         40..64 => game_state.your_hare.position as i32 * 10,
         _ => 0,
     };
+    eval += game_state.turn as i32 * 10;
     eval
 }
